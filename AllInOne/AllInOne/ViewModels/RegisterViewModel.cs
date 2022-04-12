af8905dd8bdf4f8f293897f247c382b16b700be8
alhousen <alhousen@coventry.ac.uk>
2022-04-12 15:13:32 +0100

    Checks if Email entered is valid as well as adding password policy

--- AllInOne/AllInOne/ViewModels/RegisterViewModel.cs
+++ AllInOne/AllInOne/ViewModels/RegisterViewModel.cs
@@ -2,19 +2,33 @@
 using AllInOne.Models;
 using Xamarin.Forms;
 using Firebase;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
 namespace AllInOne.ViewModels
-   
+
 {
     public class RegisterViewModel : BaseViewModel
     {
+
+        public ObservableCollection<User> Individuals { get; set; } = new ObservableCollection<User>();
+
         IAuth auth;
         private string errorMessage;
-        public string ErrorMessage { get => errorMessage;
-            set {
+
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set
+            {
                 errorMessage = value;
                 OnPropertyChanged();
             }
         }
+
 
         private string email;
         public string Email
@@ -39,30 +53,68 @@ namespace AllInOne.ViewModels
 
 
 
-    
+
         public Command RegisterCommand { protected set; get; }
         public Command RegisterButton { protected set; get; }
         public RegisterViewModel()
         {
+
             CancelCommand = new Command(OnCancel);
             RegisterCommand = new Command(OnRegisterClicked);
             auth = DependencyService.Get<IAuth>();
+        }
+        public bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        public bool ValidatePassword(string password)
+        {
+            //ValidatePassword method taken from https://social.msdn.microsoft.com/Forums/vstudio/en-US/7364a191-15cc-453e-8007-65a83e717410/password-complexity-in-c?forum=csharpgeneral#:~:text=Password%20will%20be%20match%20to,letter%2C%20and%20one%20numeric%20digit.&text=If%20you%20store%20it%2C%20the,but%20the%20Database%2Fstorage%20backend.
+            //Password must be at least 8 characters, no more than 12 characters, and must include at least one upper case letter,
+            //one lower case letter, and one numeric digit
+            string patternPassword = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$";
+            if (!string.IsNullOrEmpty(password))
+            {
+                if (!Regex.IsMatch(password, patternPassword))
+                {
+                    return false;
+                }
+
+            }
+            return true;
         }
         public async void OnRegisterClicked()
         {
             if (Email != null && Password != null)
             {
-                bool isEmail = Email.Contains("@");
-                if (isEmail)
+
+                if (IsValidEmail(Email))
                 {
                     if (Password.Length >= 8)
                     {
-                        var user = auth.Signup(email, password);
-                        if (user != null)
+                        if (ValidatePassword(Password))
                         {
-                            await App.Current.MainPage.DisplayAlert("Success", "Account created !", "OK");
-                            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+                            var user = auth.Signup(email, password);
+                            if (user != null)
+                            {
+                                await App.Current.MainPage.DisplayAlert("Success", "Account created !", "OK");
+                                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+                            }
                         }
+                        else
+                        {
+                            ErrorMessage = "Password must be at least 8 characters, no more than 12 characters, and must include at least one upper case letter,one lower case letter, and one numeric digit";
+                        }
+
                     }
                     else
                     {
@@ -71,10 +123,10 @@ namespace AllInOne.ViewModels
                 }
                 else
                 {
-                    ErrorMessage = "Please insert an Email (@ required)";
+                    ErrorMessage = "Not a valid Email address";
                 }
-                    
-               
+
+
 
 
             }
@@ -85,6 +137,8 @@ namespace AllInOne.ViewModels
 
 
         }
+
+
         public Command CancelCommand { get; }
 
         private async void OnCancel()
@@ -93,5 +147,6 @@ namespace AllInOne.ViewModels
         }
 
 
-    } }
+    }
+}
 
